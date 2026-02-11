644d53f09745fd849b88e95916a4b99a7f518c5b
hyun2ee2 <Hongkobi04@gmail.com>
2026-02-11 22:48:09 +0900

    feat: CalenderRepository

--- src/main/java/com/calenderdeepening/calender/repository/CalenderRepository.java
+++ src/main/java/com/calenderdeepening/calender/repository/CalenderRepository.java
@@ -1,12 +1,14 @@
 package com.calenderdeepening.calender.repository;
 
-import com.calenderdeepening.calender.entity.CalenderEntity;
+import com.calenderdeepening.calender.entity.Calender;
+import com.calenderdeepening.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
-public interface CalenderRepository extends JpaRepository<CalenderEntity, Long> {
+public interface CalenderRepository extends JpaRepository<Calender, Long> {
+    List<Calender> findAllByCalender(Calender calender);
 
+    List<Calender> findByUser(User user);
 }
