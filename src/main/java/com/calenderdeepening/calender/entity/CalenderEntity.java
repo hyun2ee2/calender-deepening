9de3a32035702977ab9b40f9ca4196849ab3f0ae
hyun2ee2 <hongkobi04@gmail.com>
2026-02-11 12:11:31 +0900

    feat: calender/CalenderEntity 작성자명, 할일 제목, 할일 내용, 작성일, 수정일 로직 추가

---
+++ src/main/java/com/calenderdeepening/calender/entity/CalenderEntity.java
@@ -0,0 +1,62 @@
+package com.calenderdeepening.calender.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@jakarta.persistence.Entity
+@Table(name = "calender")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class CalenderEntity {
+
+    // 사용자 ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 유저가 있어야만 게시글이 존재하기 때문에(optional은 false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    // optional과 nullable은 항상 짝이 맞아야하기 때문에 둘다 false(optional은 JPA에서 null의 허용 여부 nullable은 DB에서 null의 허용 여부)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // 작성자명(1~10사이의 텍스트 길이)
+    @Column(length = 10, nullable = false)
+    @Size(min = 1, max = 10)
+    @NotBlank
+    private String author;
+
+    // 일정 제목(1~30사이의 텍스트 길이)
+    @Column(length = 30, nullable = false)
+    @Size(min = 1, max = 30)
+    @NotBlank
+    private String title;
+
+    // 일정 내용(1~200사이의 텍스트 길이)
+    @Column(length = 200, nullable = false)
+    @Size(min = 1, max = 200)
+    @NotBlank
+    private String content;
+
+    // 일정 작성일
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    // 일정 수정일
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
