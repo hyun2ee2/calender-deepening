901383a76c4d6d8e8b38ab664461afcf81226385
hyun2ee2 <Hongkobi04@gmail.com>
2026-02-11 11:01:59 +0900

    feat: first commit

---
+++ src/main/java/com/calenderdeepening/CalenderDeepeningApplication.java
@@ -0,0 +1,13 @@
+package com.calenderdeepening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderDeepeningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenderDeepeningApplication.class, args);
+    }
+
+}
