1828ecb30be1478e425227d7d2c44898df5a3e8b
hyun2ee2 <Hongkobi04@gmail.com>
2026-02-13 02:01:28 +0900

    fix: CalenderRepository, CommentRepository Service에 맞게 로직 수정

--- src/main/java/com/calenderdeepening/calender/repository/CalenderRepository.java
+++ src/main/java/com/calenderdeepening/calender/repository/CalenderRepository.java
@@ -1,7 +1,6 @@
 package com.calenderdeepening.calender.repository;
 
 import com.calenderdeepening.calender.entity.Calender;
-import com.calenderdeepening.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
--- src/main/java/com/calenderdeepening/comment/repository/CommentRepository.java
+++ src/main/java/com/calenderdeepening/comment/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package com.calenderdeepening.comment.repository;
 
 import com.calenderdeepening.calender.entity.Calender;
 import com.calenderdeepening.comment.entity.Comment;
+import com.calenderdeepening.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,8 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByCalender(Calender calender);
+
+    List<Comment> findByUser_Id(Long userId);
+
+    Long user(User user);
 }
